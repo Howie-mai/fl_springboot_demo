70bf0d0ab22e6468b3d59be9dc503435ef0fd6a3
Howie Mai 麦皓 <Howie.Mai@tyj365.com>
2019-11-07 17:18:13 +0800

    add maven

---
+++ src/main/java/com/zhku/mh/common/utils/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.zhku.mh.common.utils;
+
+import io.jsonwebtoken.*;
+
+import java.util.Map;
+
+/**
+ * ClassName：
+ * Time：2019/11/7 17:10
+ * Description：
+ * Author： mh
+ */
+public class JwtUtil {
+    //需要key.map,颜值三个组成部分
+    //3、   签名部分
+    //根据用户信息+盐值+密钥生成的签名。
+    public static String encode(String key, Map<String, Object> param, String salt) {
+        if (salt != null && salt.length() > 0) {
+            key += salt;
+        }
+        //生成签名部分
+        JwtBuilder jwtBuilder = Jwts.builder().signWith(SignatureAlgorithm.HS256, key);
+        jwtBuilder = jwtBuilder.setClaims(param);
+        //获取生成的token
+        String token = jwtBuilder.compact();
+        return token;
+    }
+
+    public static Map<String, Object> decode(String key,String token , String salt) throws SignatureException {
+        Claims claims = null;
+        if (salt != null && salt.length() > 0) {
+            key += salt;
+        }
+
+        claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+        return claims;
+    }
+}
